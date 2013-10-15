4ef2b2143a5202cd4d65f10e66a1b1e659baf663
Sam Gross <colesbury@gmail.com>
2013-10-15 09:51:34 -0400

    Add PlayHaven 2.1.1

---
+++ playhaven-2.1.1/src/com/playhaven/android/Version.java
@@ -0,0 +1,24 @@
+package com.playhaven.android;
+
+/**
+ * Auto-generated file, contains project and version control state info.
+ */
+public final class Version {
+  public static String PLUGIN_BUILD_TIME = "2013-10-03 17:19";
+
+  public static String PROJECT_NAME = "API";
+  public static String PROJECT_GROUP = "com.playhaven.android";
+  public static String PROJECT_ARTIFACT = "playhaven-api";
+  public static String PROJECT_VERSION = "2.1.1";
+  public static String SOURCE_VERSION = "2.1.1-6-gdf9ca48";
+  public static String SOURCE_BRANCH = "release";
+  public static Boolean SOURCE_DIRTY = false;
+  public static String SOURCE_WORKING_COPY = "/Volumes/Data/jenkins/workspace/sdk-android-2-internal";
+
+  public static String SERVER_BUILD_PLAN = "sdk-android-2-internal";
+  public static String SERVER_BUILD_ID = "452";
+  public static String SERVER_BUILD_TIME = "2013-10-03_17-18-05";
+
+  public static String BANNER_FULL = "Build info: API version 2.1.1 built on 2013-10-03 17:19 source version 2.1.1-6-gdf9ca48, build id: sdk-android-2-internal-452.";
+  public static String BANNER = "2.1.1-2.1.1-6-gdf9ca48 2013-10-03 17:19 ";
+}
